113c577bb6cb23e2b062ea7ce95ec43e09a99923
ThisseasX <thisseasx@yahoo.com>
2018-02-21 00:38:25 +0200

    Responsive Login

--- VoteApp/web/js/script.js
+++ VoteApp/web/js/script.js
@@ -72,7 +72,9 @@ function myFilter() {
 
     // Loop through all table rows, and hide those who don't match the search query
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[2];
+        var elements = tr[i].getElementsByTagName("td");
+        var surnameIndex = elements.length - 2;
+        td = elements[surnameIndex];
         if (td) {
             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
