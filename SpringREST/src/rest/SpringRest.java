c587497e081e2178086c73c7c1562927d1ffec23
Thisseas X <thisseasx@yahoo.com>
2018-04-01 05:14:13 +0300

    SpringREST wip

--- SpringREST/src/rest/SpringRest.java
+++ SpringREST/src/rest/SpringRest.java
@@ -1,5 +1,7 @@
 package rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringRest {
 
     @GetMapping("/test")
-    public String getAll() {
-        return "hello";
+    public ResponseEntity getAll() {
+        return new ResponseEntity<>("Hello!", HttpStatus.OK);
     }
 }
--- SpringREST/web/WEB-INF/jsp/hello.jsp
+++ SpringREST/web/WEB-INF/jsp/hello.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
-  User: thiss
-  Date: 27/3/2018
-  Time: 7:15 μμ
+  User: Theseus
+  Date: 1/4/2018
+  Time: 4:29 πμ
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-  <title>Index</title>
+  <title>Title</title>
 </head>
 <body>
-Index
+yolo
 </body>
 </html>
